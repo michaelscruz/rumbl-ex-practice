a19d4c49edddaf72b52e951c13f61722fe04026c
Michael Cruz <michael@oakroot.net>
2022-12-13 16:40:52 -0500

    Add chapter 7 changes

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -6,8 +6,19 @@ defmodule Rumbl.Multimedia do
   import Ecto.Query, warn: false
   alias Rumbl.Repo
 
+  alias Rumbl.Multimedia.Category
   alias Rumbl.Multimedia.Video
   alias Rumbl.Accounts
+
+  def create_category!(name) do
+    Repo.insert!(%Category{name: name}, on_conflict: :nothing)
+  end
+
+  def list_alphabetical_categories do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
 
   @doc """
   Returns the list of videos.

e8b51b8a4af959dc9d637c720d73ba66b3f59266
Michael Cruz <michael@oakroot.net>
2023-01-09 14:23:19 -0500

    Add integ test and view test

--- config/test.exs
+++ config/test.exs
@@ -28,3 +28,6 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Set password to hash for one round only in test to keep things moving quickly
+config :pbkdf2_elixir, :rounds, 1
